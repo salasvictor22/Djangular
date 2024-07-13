9a82b948eb0d7ddfaa47a1225cbbd3eb33fcb785
salasvictor22 <jsalasag@unsa.edu.pe>
2024-07-13 11:27:12 -0500

    se modifico algunos archivos del proyecto de angular para solucionar algunos errores

--- crud/src/app/app.component.ts
+++ crud/src/app/app.component.ts
@@ -1,23 +1,31 @@
 import { Component } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-  providers: [ApiService]
+  styleUrls: ['./app.component.css'],
+  providers: [ApiService] 
 })
 export class AppComponent {
-  movies = [{id:1,title:'peli1',year:2021},{id:2,title:'peli2',year:2022}];
-  constructor(private api:ApiService) {
+  movies = [
+    { id: 1, title: 'peli1', year: 2021 },
+    { id: 2, title: 'peli2', year: 2022 }
+  ];
+
+  constructor(private api: ApiService) {
     this.getMovies();
   }
+
   getMovies = () => {
-    this.api.getAllMovies().subscribe (
+    this.api.getAllMovies().subscribe(
       data => {
         console.log(data);
-        //this.movies = data;  //data.results;
+        //this.movies = data;  //data.results; // Descomentar si tienes datos
       },
       error => {
         console.log(error);
-  } )  } }
-
+      }
+    );
+  }
+}
--- crud/src/app/app.module.ts
+++ crud/src/app/app.module.ts
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
 	HttpClientModule,
   ],
   providers: [],
