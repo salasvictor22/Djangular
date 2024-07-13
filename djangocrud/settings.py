7481d8d0f72c787a3371412d63678f88a2ec1f97
salasvictor22 <jsalasag@unsa.edu.pe>
2024-07-13 11:39:21 -0500

    se instalo CORS-HEADERS para corregir problema de No-Access-Control-Allow-Origin y tambien se modifico el archivo settings.py para que funcione correctamente

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api',
     'rest_framework',
-
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'djangocrud.urls'
@@ -124,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200",
+]
